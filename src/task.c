1b59fdaf5ab2ad1336fae2316da451c6e5463db7
Juuso Alasuutari <juuso.alasuutari@gmail.com>
2026-05-25 21:00:00 +0300

    task: remove unused includes

--- src/task.c
+++ src/task.c
@@ -5,12 +5,8 @@
 
 #include "compat.h"
 
-#include <errno.h>
 #include <inttypes.h>
 #include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 #include "bits.h"
 #include "task.h"
